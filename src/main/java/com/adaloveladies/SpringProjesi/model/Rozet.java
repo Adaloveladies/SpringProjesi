06027b69f85b91f76cc09b25f0094cdb04ee8a70
Meryem <meeryemtekeli@gmail.com>
2025-06-18 13:20:48 +0300

    fix: Rozet modelinde güncelleme yapıldı

--- src/main/java/com/adaloveladies/SpringProjesi/model/Rozet.java
+++ src/main/java/com/adaloveladies/SpringProjesi/model/Rozet.java
@@ -3,7 +3,7 @@ package com.adaloveladies.SpringProjesi.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
